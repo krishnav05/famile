b2a6d26914f80837e96ff665cf656666f01f9d2e
krishna <krishna.fnatic@gmail.com>
2020-08-22 17:09:16 +0530

    phone

--- routes/api.php
+++ routes/api.php
@@ -91,7 +91,7 @@ File::makeDirectory($path, $mode = 0777, true, true);
 
     Route::post('editprofile',function(){
 
-    	Profile::where('id',$_POST['editid'])->update(['name'=>$_POST['name'],'age'=>$_POST['age'],'blood_group'=>$_POST['blood'],'height'=>$_POST['height'],'weight'=>$_POST['weight'],'occupation'=>$_POST['occupation'],'email'=>$_POST['email'],'mobile'=>$_POST['mobile'],'city'=>$_POST['city'],'state'=>$_POST['state'],'address'=>$_POST['address']]);
+    	Profile::where('id',$_POST['editid'])->update(['name'=>$_POST['name'],'age'=>$_POST['age'],'blood_group'=>$_POST['blood'],'height'=>$_POST['height'],'weight'=>$_POST['weight'],'occupation'=>$_POST['occupation'],'email'=>$_POST['email'],'phone'=>$_POST['mobile'],'city'=>$_POST['city'],'state'=>$_POST['state'],'address'=>$_POST['address']]);
 
     	if($_POST['photo'] !== null){
     		$image = $_POST['image'];
