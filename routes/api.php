846ec864b75fc95702b6fa68432798cbf4e71a88
Krishna Verma <48890831+krishnav05@users.noreply.github.com>
2020-11-24 16:45:31 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,7 @@ return response()
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
-              CURLOPT_URL => "https://2factor.in/API/V1/7f3c1f82-0d1b-11eb-9fa5-0200cd936042/SMS/".$_POST['phone']."/".$otp."/AUTOVERIFY",
+              CURLOPT_URL => "https://2factor.in/API/V1/7f3c1f82-0d1b-11eb-9fa5-0200cd936042/SMS/".$_POST['phone']."/".$otp."/AUTOVERIFY2",
               CURLOPT_RETURNTRANSFER => true,
               CURLOPT_ENCODING => "",
               CURLOPT_MAXREDIRS => 10,
@@ -173,7 +173,7 @@ return response()
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
-              CURLOPT_URL => "https://2factor.in/API/V1/7f3c1f82-0d1b-11eb-9fa5-0200cd936042/SMS/".$_POST['phone']."/".$otp."/AUTOVERIFY",
+              CURLOPT_URL => "https://2factor.in/API/V1/7f3c1f82-0d1b-11eb-9fa5-0200cd936042/SMS/".$_POST['phone']."/".$otp."/AUTOVERIFY2",
               CURLOPT_RETURNTRANSFER => true,
               CURLOPT_ENCODING => "",
               CURLOPT_MAXREDIRS => 10,
