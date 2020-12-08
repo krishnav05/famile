9f5ea021be334941151e0b623638fc558a45481b
Krishna Verma <48890831+krishnav05@users.noreply.github.com>
2020-12-08 16:04:24 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -305,11 +305,10 @@ File::makeDirectory($path, $mode = 0777, true, true);
         $new = Documents::where('profile_id',$_POST['profileid'])->orderBy('id', 'DESC')->get();
         $image_links = array();
         foreach($new as $n){
-            $string = 'https://app.famile.care/prescriptions/'+$n->profile_id+'/'+$n->document;
+            $string = 'https://app.famile.care/prescriptions/'.$n->profile_id.'/'.$n->document;
             array_push($image_links, $string);
         }
-        $new->image_links = $image_links;
-        return response()->json($new);
+        return response()->json($image_links);
     });
 
     Route::post('setlocation',function(){
