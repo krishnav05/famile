ea63f13dc90772a2e4646ab5b838dfb9ea803109
krishna <krishna.fnatic@gmail.com>
2020-12-17 21:11:58 +0530

    qr1

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -12,6 +12,22 @@
 </head>
 <body>
   <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+  <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
     @foreach($profiles as $profile)
     @if ($loop->first)
     <li class="nav-item">
@@ -97,15 +113,6 @@
   -->
 </body>
 <script type="text/javascript">
-  var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
-  triggerTabList.forEach(function (triggerEl) {
-    var tabTrigger = new bootstrap.Tab(triggerEl)
-
-    triggerEl.addEventListener('click', function (event) {
-      event.preventDefault()
-      tabTrigger.show()
-    })
-  })
   $('.carousel').carousel({
   wrap: false
   });
