a0ff29b416712eeeb0a309a02fa787f632cbb498
krishna <krishna.fnatic@gmail.com>
2020-12-17 22:58:31 +0530

    qr1

--- app/User.php
+++ app/User.php
@@ -10,7 +10,6 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use Notifiable,HasApiTokens;
-    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
