431643285c49d12c8b1f5f1be35e6cffe28f4538
krishna <krishna.fnatic@gmail.com>
2020-12-17 22:39:56 +0530

    qr1

---
+++ app/Console/Commands/DemoCron.php
@@ -0,0 +1,55 @@
+<?php
+   
+namespace App\Console\Commands;
+   
+use Illuminate\Console\Command;
+use App\User;
+   
+class DemoCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'demo:cron';
+    
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \Log::info("Cron is working fine!");
+        $users = User::get();
+        foreach ($users as $user) {
+            # code...
+            $random = rand(1000,9999);
+            User::where('id',$user->id)->update(['sharecode',$random]);
+        }
+        /*
+           Write your database logic we bellow:
+           Item::create(['name'=>'hello new']);
+        */
+      
+        $this->info('Demo:Cron Cummand Run successfully!');
+    }
+}
