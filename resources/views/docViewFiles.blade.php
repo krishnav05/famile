9a4a4f4f632249a5ab5018cab5e6e56a94288955
krishna <krishna.fnatic@gmail.com>
2020-12-17 22:18:42 +0530

    qr1

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -32,7 +32,7 @@
         <div class="carousel-inner">
           @foreach($documents as $document)
           @if($document['profile_id'] == $profile['id'])
-          <div class="carousel-item active">
+          <div class="carousel-item">
             <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
           </div>
           @endif
@@ -54,7 +54,7 @@
         <div class="carousel-inner">
           @foreach($documents as $document)
           @if($document['profile_id'] == $profile['id'])
-          <div class="carousel-item active">
+          <div class="carousel-item">
             <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
           </div>
           @endif
@@ -85,5 +85,13 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
   -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    @foreach($profiles as $profile)
+    $(document).ready(function () {
+      $('#carouselExampleControls{{str_replace(' ', '', $profile['name'])}}').find('.item').first().addClass('active');
+    });
+    @endforeach
+  </script>
 </body>
 </html>
