42e3be9b4c1e38960db54be53da5964cd278093b
krishna <krishna.fnatic@gmail.com>
2020-12-17 21:24:17 +0530

    qr1

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -173,9 +173,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
   -->
 </body>
-<script type="text/javascript">
-  $('.carousel').carousel({
-  wrap: false
-  });
-</script>
 </html>
