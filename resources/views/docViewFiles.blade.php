bcafb56e3b5b54c9c2843de1c7730ba65d70d984
krishna <krishna.fnatic@gmail.com>
2020-12-17 21:18:03 +0530

    qr1

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -12,20 +12,78 @@
 </head>
 <body>
   <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  </li>
-</ul>
+    @foreach($profiles as $profile)
+    @if ($loop->first)
+    <li class="nav-item" role="presentation">
+      <a class="nav-link active" id="{{$profile['id']}}-tab" data-bs-toggle="tab" href="#{{$profile['id']}}" role="tab" aria-controls="{{$profile['id']}}" aria-selected="true">{{$profile['name']}}</a>
+    </li>
+    @else
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" id="{{$profile['id']}}-tab" data-bs-toggle="tab" href="#{{$profile['id']}}" role="tab" aria-controls="{{$profile['id']}}" aria-selected="false">{{$profile['name']}}</a>
+    </li>
+    @endif
+    @endforeach
+  </ul>
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+  @foreach($profiles as $profile)
+  @if ($loop->first)
+  <div class="tab-pane fade show active" id="{{$profile['id']}}" role="tabpanel" aria-labelledby="{{$profile['id']}}-tab">
+    <div id="carouselExampleControls{{$profile['id']}}" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          @foreach($documents as $document)
+          @if($document['profile_id'] == $profile['id'])
+          @if ($loop->first)
+          <div class="carousel-item active">
+            <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
+          </div>
+          @else
+          <div class="carousel-item">
+            <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
+          </div>
+          @endif
+          @endif
+          @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls{{$profile['id']}}" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls{{$profile['id']}}" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </a>
+      </div>
+  </div>
+  @else
+  <div class="tab-pane fade" id="{{$profile['id']}}" role="tabpanel" aria-labelledby="{{$profile['id']}}-tab">
+    <div id="carouselExampleControls{{$profile['id']}}" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          @foreach($documents as $document)
+          @if($document['profile_id'] == $profile['id'])
+          @if ($loop->first)
+          <div class="carousel-item active">
+            <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
+          </div>
+          @else
+          <div class="carousel-item">
+            <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
+          </div>
+          @endif
+          @endif
+          @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls{{$profile['id']}}" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls{{$profile['id']}}" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </a>
+      </div>
+  </div>
+  @endif
+  @endforeach
 </div>
   <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
     @foreach($profiles as $profile)
