3c7e0f2cb525c5fc5abeead5dfe16c9280f11bdf
krishna <krishna.fnatic@gmail.com>
2020-12-17 21:05:57 +0530

    qr1

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -26,12 +26,11 @@
   </ul>
   <div class="tab-content" id="myTabContent">
     @foreach($profiles as $profile)
-    @if ($loop->first)
     <div class="tab-pane fade show active" id="{{$profile['id']}}" role="tabpanel">
       <div id="carouselExampleControls{{$profile['id']}}" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           @foreach($documents as $document)
-          @if($document['profile_id' == $profile['id']])
+          @if($document['profile_id'] == $profile['id']])
           @if ($loop->first)
           <div class="carousel-item active">
             <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
@@ -59,7 +58,7 @@
       <div id="carouselExampleControls{{$profile['id']}}" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           @foreach($documents as $document)
-          @if($document['profile_id' == $profile['id']])
+          @if($document['profile_id'] == $profile['id']])
           @if ($loop->first)
           <div class="carousel-item active">
             <img src="/prescriptions/{{$profile['id']}}/{{$document['document']}}" class="d-block w-100">
