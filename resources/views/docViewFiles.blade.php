c76f83c22d99edd01b9a08e6311ef4e60ee208df
krishna <krishna.fnatic@gmail.com>
2020-12-23 20:12:45 +0530

    change

--- resources/views/docViewFiles.blade.php
+++ resources/views/docViewFiles.blade.php
@@ -4,18 +4,10 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style type="text/css">
-    .carousel-control.left {
-  margin-left: -25px;
-}
-
-.carousel-control.right {
-  margin-right: -25px;
-}
-  </style>
+  
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
   <title>Famile</title>
 </head>
 <body>
@@ -48,11 +40,11 @@
           @endforeach
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls{{str_replace(' ', '', $profile['name'])}}" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <i class="fas fa-arrow-left"></i>
           <span class="visually-hidden">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls{{str_replace(' ', '', $profile['name'])}}" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <i class="fas fa-arrow-right"></i>
           <span class="visually-hidden">Next</span>
         </a>
       </div>
@@ -70,11 +62,11 @@
           @endforeach
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls{{str_replace(' ', '', $profile['name'])}}" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <i class="fas fa-arrow-left"></i>
           <span class="visually-hidden">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls{{str_replace(' ', '', $profile['name'])}}" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <i class="fas fa-arrow-right"></i>
           <span class="visually-hidden">Next</span>
         </a>
       </div>
