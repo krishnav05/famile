b653650958a28b33b56fd80ffed03cc9eddae46e
krishna <krishna.fnatic@gmail.com>
2021-03-08 18:38:43 +0530

    autoverify

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -152,7 +152,8 @@ class ProfileController extends Controller
             "registration_ids" => $firebaseToken,
             "notification" => [
                 "title" => "Dear User",
-                "body" => $request->pushnotification,  
+                "body" => $request->pushnotification, 
+                "image" => 'https://app.famile.care/assets/img/famile.png'  
             ]
         ];
         $dataString = json_encode($data);
