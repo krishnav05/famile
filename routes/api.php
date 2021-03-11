79fb7e2b7f2fb4914c87df47e982ad06e4915dd2
krishna <krishna.fnatic@gmail.com>
2021-03-11 14:29:25 +0530

    data

--- routes/api.php
+++ routes/api.php
@@ -445,7 +445,7 @@ File::makeDirectory($path, $mode = 0777, true, true);
     });
 
     Route::post('getprofilechartdatadetail',function(Request $request){
-     $data = ConvertedPrescription::where('profile_id',$request->profile_id)->whereYear('consultation_date', $request->year)
+     $data = ConvertedPrescription::where('profile_id',$request->profile_id)->select(*,DB::raw("MONTHNAME(consultation_date) as monthname"))->whereYear('consultation_date', $request->year)
 ->groupBy('monthname')
 ->get();
 
